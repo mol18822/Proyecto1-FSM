936885a3c5f1e01bb30803d4c6122b639c0ab8c8
mol18822 <68083402+LuispedroMolina@users.noreply.github.com>
2020-10-16 21:41:35 -0600

    Entrega final.

--- Progra_DIGI/FSM.v
+++ Progra_DIGI/FSM.v
@@ -32,11 +32,26 @@ module Timer (input wire ENABLE, output reg TIME);
     end
 endmodule
 
-module FSM_BIENVENIDA (input wire clk, reset, output wire [2:0]F, output wire [2:0]A, input wire ON, input wire CONT, input wire TIME, input wire [1:0]SELE, output wire [2:0]DISP, output wire [2:0]MENU, output wire ENABLE);
+// Timer 2
 
-    FlipFloptipoD3Bits U1(F, clk, reset, A); 
+module Timer2 (input wire ENABLE1, output reg TIME2);
+    always @ (ENABLE1) begin
+        if (ENABLE1 == 0) begin
+            TIME2 = 0 ;
+        end while(ENABLE1) begin
+            TIME2 = 0;
+            #3 TIME2 = 1 ;
+            #3 TIME2 = 0 ;
+        end
+
+    end
+endmodule
 
 // INTERFAZ DE BIENVENIDA - SELECCIÓN DE MENÚS 
+
+module FSM_BIENVENIDA (input wire clk, reset, output wire [2:0]F, output wire [2:0]A, input wire ON, input wire CONT, input wire TIME, input wire [1:0]SELE, output wire [2:0]MENU, output wire ENABLE);
+
+    FlipFloptipoD3Bits U1(F, clk, reset, A); 
 
     assign F[2]    = (~A[2] & A[1] & ~A[0] & SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[0]);
     assign F[1]    = (~A[2] & ~A[1] & A[0] & CONT) | (~A[2] & A[1] & ~A[0] & SELE[1] & SELE[0]) | (~A[2] & A[1] & ~A[0] & ~SELE[1] & ~SELE[0]);
@@ -44,78 +59,117 @@ module FSM_BIENVENIDA (input wire clk, reset, output wire [2:0]F, output wire [2
     assign MENU[2] = (A[2] & ~A[1] & ~A[0]);
     assign MENU[1] = (A[2] & ~A[1] & A[0]);
     assign MENU[0] = (A[2] & A[1] & ~A[0]);
-    assign DISP[2] = (A[2] & ~A[1]) | (A[2] & ~A[0]);
-    assign DISP[1] = (A[2] & A[1] & ~A[0]);
-    assign DISP[0] = (A[2] & ~A[1] & A[0]);
     assign ENABLE  = (~A[2] & ~A[1] & A[0]);
 
 endmodule
-
-module FSM_MENU1 (input wire clk, reset, output wire [2:0]F, output wire [2:0]A, input wire [2:0]MENU, input wire CONT, input wire TIME, input wire [2:0]SELE, output wire [2:0]DISP, output wire [3:0]OP, output wire ENABLE);
-
-    DFlipFlop3bits U2 (F, clk, reset, A);
 
 // INTERFAZ DE MENÚ 1 - PLATOS FUERTES
 
-    assign F[2]    = (~A[2] & A[1] & ~A[0] & ~SELE[2] & SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[2] & ~SELE[1] & ~SELE[0]);
-    assign F[1]    = (~A[2] & ~A[1] & A[0] & CONT) | (~A[2] & A[1] & ~A[0] & ~SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[2] & ~SELE[0]);
-    assign F[0]    = (~A[2] & ~A[1] & ~A[0] & ON) | (~A[2] & ~A[1] & A[0] & ~CONT & ~TIME) | (~A[2] & A[1] & ~A[0] & ~SELE[2] & SELE[0]) | (~A[2] & A[1] & ~A[0] & SELE[1] & SELE[0]);
-    assign OP[3]   = (~A[2] & A[1] & A[0]);
-    assign OP[2]   = (A[2] & ~A[1] & ~A[0]);
-    assign OP[1]   = (A[2] & ~A[1] & A[0]);
-    assign OP[0]   = (A[2] & A[1] & ~A[0]);
-    assign DISP[3] = (~A[2] & A[1] & A[0]);
-    assign DISP[2] = (A[2] & ~A[1] & ~A[0]);
-    assign DISP[1] = (A[2] & ~A[1] & A[0]);
-    assign DISP[0] = (A[2] & A[1] & ~A[0]);
-    assign ENABLE  = (~A[2] & ~A[1] & A[0]);
+module FSM_MENU1 (input wire clk, reset, output wire [2:0]Fm1, output wire [2:0]Am1, input wire [2:0]MENU, input wire CONTm1, input wire TIME, input wire [2:0]SELEm1, output wire [3:0]OP, output wire ENABLE);
 
+    FlipFloptipoD3Bits U2 (Fm1, clk, reset, Am1);
+    
+    assign Fm1[2]    = (~Am1[2] & Am1[1] & ~Am1[0] & ~SELEm1[2] & SELEm1[1]) | (~Am1[2] & Am1[1] & ~Am1[0] & SELEm1[2] & ~SELEm1[1] & ~SELEm1[0]);
+    assign Fm1[1]    = (~Am1[2] & ~Am1[1] & Am1[0] & CONTm1) | (~Am1[2] & Am1[1] & ~Am1[0] & ~SELEm1[1]) | (~Am1[2] & Am1[1] & ~Am1[0] & SELEm1[2] & ~SELEm1[0]);
+    assign Fm1[0]    = (~Am1[2] & ~Am1[1] & Am1[0] & ~CONTm1 & ~TIME) | (~Am1[2] & Am1[1] & ~Am1[0] & ~SELEm1[2] & SELEm1[0]) | (~Am1[2] & Am1[1] & ~Am1[0] & SELEm1[1] & SELEm1[0]) | (~Am1[2] & ~Am1[1] & ~Am1[0] & MENU[2] & ~MENU[1] & ~MENU[0]);
+    assign OP[3]     = (~Am1[2] & Am1[1] & Am1[0]);
+    assign OP[2]     = (Am1[2] & ~Am1[1] & ~Am1[0]);
+    assign OP[1]     = (Am1[2] & ~Am1[1] & Am1[0]);
+    assign OP[0]     = (Am1[2] & Am1[1] & ~Am1[0]);
+    assign ENABLE    = (~Am1[2] & ~Am1[1] & Am1[0]);
 
 endmodule
-
-/*module FMS_MENU2 (input wire clk, reset, input wire [2:0]F, input wire [2:0]A, input wire CONT, TIME, [2:0]SELE, output wire [3:0]DISP, [3:0]G);
-
-    DFlipFlop3bits U3 (F, clk, reset, A);
 
 // INTERFAZ DE MENÚ 2 - GUARNICIONES
 
-    assign F[2]    = (~A[2] & A[1] & ~A[0] & ~SELE[2] & SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[2] & ~SELE[1] & ~SELE[0]);
-    assign F[1]    = (~A[2] & ~A[1] & A[0] & CONT) | (~A[2] & A[1] & ~A[0] & ~SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[2] & ~SELE[0]);
-    assign F[0]    = (~A[2] & ~A[1] & ~A[0] & ON) | (~A[2] & ~A[1] & A[0] & ~CONT & ~TIME) | (~A[2] & A[1] & ~A[0] & ~SELE[2] & SELE[0]) | (~A[2] & A[1] & ~A[0] & SELE[1] & SELE[0]);
-    assign G[3]    = (~A[2] & A[1] & A[0]);
-    assign G[2]    = (A[2] & ~A[1] & ~A[0]);
-    assign G[1]    = (A[2] & ~A[1] & A[0]);
-    assign G[0]    = (A[2] & A[1] & ~A[0]);
-    assign DISP[3] = (~A[2] & A[1] & A[0]);
-    assign DISP[2] = (A[2] & ~A[1] & ~A[0]);
-    assign DISP[1] = (A[2] & ~A[1] & A[0]);
-    assign DISP[0] = (A[2] & A[1] & ~A[0]);
-    assign ENABLE  = (~A[2] & ~A[1] & A[0]);
+module FSM_MENU2 (input wire clk, reset, output wire [2:0]Fm2, output wire [2:0]Am2, input wire [3:0]OP, input wire CONTm2, input wire TIME, input wire [2:0]SELEm2, output wire [3:0]G, output wire ENABLE);
+
+    FlipFloptipoD3Bits U3 (Fm2, clk, reset, Am2);
+
+    assign Fm2[2]  = (~Am2[2] & Am2[1] & ~Am2[0] & ~SELEm2[2] & SELEm2[1]) | (~Am2[2] & Am2[1] & ~Am2[0] & SELEm2[2] & ~SELEm2[1] & ~SELEm2[0]);
+    assign Fm2[1]  = (~Am2[2] & ~Am2[1] & Am2[0] & CONTm2) | (~Am2[2] & Am2[1] & ~Am2[0] & ~SELEm2[1]) | (~Am2[2] & Am2[1] & ~Am2[0] & SELEm2[2] & ~SELEm2[0]);
+    assign Fm2[0]  = (~Am2[2] & ~Am2[1] & Am2[0] & ~CONTm2 & ~TIME) | (~Am2[2] & Am2[1] & ~Am2[0] & ~SELEm2[2] & SELEm2[0]) | (~Am2[2] & Am2[1] & ~Am2[0] & SELEm2[1] & SELEm2[0]) | (~Am2[2] & ~Am2[1] & ~Am2[0] & ~OP[3] & ~OP[2] & ~OP[1] & OP[0]) | (~Am2[2] & ~Am2[1] & ~Am2[0] & ~OP[3] & ~OP[2] & OP[1] & ~OP[0]) | (~Am2[2] & ~Am2[1] & ~Am2[0] & ~OP[3] & OP[2] & ~OP[1] & ~OP[0]) | (~Am2[2] & ~Am2[1] & ~Am2[0] & OP[3] & ~OP[2] & ~OP[1] & ~OP[0]);
+    assign G[3]    = (~Am2[2] & Am2[1] & Am2[0]);
+    assign G[2]    = (Am2[2] & ~Am2[1] & ~Am2[0]);
+    assign G[1]    = (Am2[2] & ~Am2[1] & Am2[0]);
+    assign G[0]    = (Am2[2] & Am2[1] & ~Am2[0]);
+    assign ENABLE  = (~Am2[2] & ~Am2[1] & Am2[0]);
 
 endmodule
 
-module FMS_MENU3 (input wire clk, reset, input wire [2:0]F, input wire [2:0]A, input wire CONT, TIME, [2:0]SELE, output wire [3:0]DISP, [3:0]B);
-
-    DFlipFlop3bits U4 (F, clk, reset, A);
-    
 // INTERFAZ DE MENÚ 3 - BEBIDAS
 
-    assign F[2]    = (~A[2] & A[1] & ~A[0] & ~SELE[2] & SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[2] & ~SELE[1] & ~SELE[0]);
-    assign F[1]    = (~A[2] & ~A[1] & A[0] & CONT) | (~A[2] & A[1] & ~A[0] & ~SELE[1]) | (~A[2] & A[1] & ~A[0] & SELE[2] & ~SELE[0]);
-    assign F[0]    = (~A[2] & ~A[1] & ~A[0] & ON) | (~A[2] & ~A[1] & A[0] & ~CONT & ~TIME) | (~A[2] & A[1] & ~A[0] & ~SELE[2] & SELE[0]) | (~A[2] & A[1] & ~A[0] & SELE[1] & SELE[0]);
-    assign B[3]    = (~A[2] & A[1] & A[0]);
-    assign B[2]    = (A[2] & ~A[1] & ~A[0]);
-    assign B[1]    = (A[2] & ~A[1] & A[0]);
-    assign B[0]    = (A[2] & A[1] & ~A[0]);
-    assign DISP[3] = (~A[2] & A[1] & A[0]);
-    assign DISP[2] = (A[2] & ~A[1] & ~A[0]);
-    assign DISP[1] = (A[2] & ~A[1] & A[0]);
-    assign DISP[0] = (A[2] & A[1] & ~A[0]);
-    assign ENABLE  = (~A[2] & ~A[1] & A[0]);
+module FSM_MENU3 (input wire clk, reset, output wire [2:0]Fm3, output wire [2:0]Am3, input wire [3:0]G, input wire CONTm3, input wire TIME, input wire [2:0]SELEm3, output wire [3:0]B, output wire ENABLE);
+
+    FlipFloptipoD3Bits U4 (Fm3, clk, reset, Am3);
+
+    assign Fm3[2]  = (~Am3[2] & Am3[1] & ~Am3[0] & ~SELEm3[2] & SELEm3[1]) | (~Am3[2] & Am3[1] & ~Am3[0] & SELEm3[2] & ~SELEm3[1] & ~SELEm3[0]);
+    assign Fm3[1]  = (~Am3[2] & ~Am3[1] & Am3[0] & CONTm3) | (~Am3[2] & Am3[1] & ~Am3[0] & ~SELEm3[1]) | (~Am3[2] & Am3[1] & ~Am3[0] & SELEm3[2] & ~SELEm3[0]);
+    assign Fm3[0]  = (~Am3[2] & ~Am3[1] & Am3[0] & ~CONTm3 & ~TIME) | (~Am3[2] & Am3[1] & ~Am3[0] & ~SELEm3[2] & SELEm3[0]) | (~Am3[2] & Am3[1] & ~Am3[0] & SELEm3[1] & SELEm3[0]) | (~Am3[2] & ~Am3[1] & ~Am3[0] & ~G[3] & ~G[2] & ~G[1] & G[0]) | (~Am3[2] & ~Am3[1] & ~Am3[0] & ~G[3] & ~G[2] & G[1] & ~G[0]) | (~Am3[2] & ~Am3[1] & ~Am3[0] & ~G[3] & G[2] & ~G[1] & ~G[0]) | (~Am3[2] & ~Am3[1] & ~Am3[0] & G[3] & ~G[2] & ~G[1] & ~G[0]);
+    assign B[3]    = (~Am3[2] & Am3[1] & Am3[0]);
+    assign B[2]    = (Am3[2] & ~Am3[1] & ~Am3[0]);
+    assign B[1]    = (Am3[2] & ~Am3[1] & Am3[0]);
+    assign B[0]    = (Am3[2] & Am3[1] & ~Am3[0]);
+    assign ENABLE  = (~Am3[2] & ~Am3[1] & Am3[0]);
+
+endmodule 
+
+module FSM_B1 (input wire clk, reset, output wire [2:0]FB1, output wire [2:0]AB1, input wire [3:0]B, input wire [1:0]SELEB1, output wire [1:0]SB1);
+
+    FlipFloptipoD3Bits U5 (FB1, clk, reset, AB1);
+
+    assign FB1[2]  = (0);
+    assign FB1[1]  = (~AB1[2] & ~AB1[1] & AB1[0] & ~SELEB1[1] & SELEB1[0]) | (~AB1[2] & ~AB1[1] & AB1[0] & SELEB1[1] & ~SELEB1[0]);
+    assign FB1[0]  = (~AB1[2] & ~AB1[1] & AB1[0] & SELEB1[1]) | (~AB1[2] & ~AB1[1] & AB1[0] & ~SELEB1[0]) | (~AB1[2] & ~AB1[1] & ~AB1[0] & B[3] & ~B[2] & ~B[1] & ~B[0]);
+    assign SB1[1] = (~AB1[2] & ~AB1[1] & AB1[0] & SELEB1[1] & ~SELEB1[0]);
+    assign SB1[0] = (~AB1[2] & ~AB1[1] & AB1[0] & ~SELEB1[1] & SELEB1[0]);
+    
+endmodule
+
+module FSM_B2 (input wire clk, reset, output wire [2:0]FB2, output wire [2:0]AB2, input wire [3:0]B, input wire [1:0]SELEB2, output wire [1:0]SB2);
+
+    FlipFloptipoD3Bits U6 (FB2, clk, reset, AB2);
+
+    assign FB2[2]  = (0);
+    assign FB2[1]  = (~AB2[2] & ~AB2[1] & AB2[0] & ~SELEB2[1] & SELEB2[0]) | (~AB2[2] & ~AB2[1] & AB2[0] & SELEB2[1] & ~SELEB2[0]);
+    assign FB2[0]  = (~AB2[2] & ~AB2[1] & AB2[0] & SELEB2[1]) | (~AB2[2] & ~AB2[1] & AB2[0] & ~SELEB2[0]) | (~AB2[2] & ~AB2[1] & ~AB2[0] & ~B[3] & B[2] & ~B[1] & ~B[0]);
+    assign SB2[1] = (~AB2[2] & ~AB2[1] & AB2[0] & SELEB2[1] & ~SELEB2[0]);
+    assign SB2[0] = (~AB2[2] & ~AB2[1] & AB2[0] & ~SELEB2[1] & SELEB2[0]);
+
+endmodule 
+
+module FSM_B3 (input wire clk, reset, output wire [2:0]FB3, output wire [2:0]AB3, input wire [3:0]B, input wire [1:0]SELEB3, output wire [1:0]SB3);
+
+    FlipFloptipoD3Bits U7 (FB3, clk, reset, AB3);
+
+    assign FB3[2] = (0);
+    assign FB3[1] = (~AB3[2] & ~AB3[1] & AB3[0] & ~SELEB3[1] & SELEB3[0]) | (~AB3[2] & ~AB3[1] & AB3[0] & SELEB3[1] & ~SELEB3[0]);
+    assign FB3[0] = (~AB3[2] & ~AB3[1] & AB3[0] & SELEB3[1]) | (~AB3[2] & ~AB3[1] & AB3[0] & ~SELEB3[0]) | (~AB3[2] & ~AB3[1] & ~AB3[0] & ~B[3] & ~B[2] & B[1] & ~B[0]);
+    assign SB3[1] = (~AB3[2] & ~AB3[1] & AB3[0] & SELEB3[1] & ~SELEB3[0]);
+    assign SB3[0] = (~AB3[2] & ~AB3[1] & AB3[0] & ~SELEB3[1] & SELEB3[0]);
+
+endmodule 
+
+module FSM_B4 (input wire clk, reset, output wire [2:0]FB4, output wire [2:0]AB4, input wire [3:0]B, input wire [1:0]SELEB4, output wire [1:0]SB4);
+
+    FlipFloptipoD3Bits U8 (FB4, clk, reset, AB4);
+
+    assign FB4[2] = (0);
+    assign FB4[1] = (~AB4[2] & ~AB4[1] & AB4[0] & ~SELEB4[1] & SELEB4[0]) | (~AB4[2] & ~AB4[1] & AB4[0] & SELEB4[1] & ~SELEB4[0]);
+    assign FB4[0] = (~AB4[2] & ~AB4[1] & AB4[0] & SELEB4[1]) | (~AB4[2] & ~AB4[1] & AB4[0] & ~SELEB4[0]) | (~AB4[2] & ~AB4[1] & ~AB4[0] & ~B[3] & ~B[3] & ~B[1] & B[0]);
+    assign SB4[1] = (~AB4[2] & ~AB4[1] & AB4[0] & SELEB4[1] & ~SELEB4[0]);
+    assign SB4[0] = (~AB4[2] & ~AB4[1] & AB4[0] & ~SELEB4[1] & SELEB4[0]);
 
 endmodule
-*/
 
+module FSM_LLENADOB1(input wire clk, reset, output wire [2:0]FF1, output wire [2:0]AF1, input wire TIME, input wire TIME2, input wire [1:0]SB1, input wire M1, output wire DONE, output wire ENABLE1);
 
+    FlipFloptipoD3Bits U9 (FF1, clk, reset, AF1);
 
+    assign FF1[2] = (AF1[2] & ~AF1[1] & ~AF1[0]) | (~AF1[2] & AF1[1] & ~AF1[0] & M1) | (~AF1[2] & AF1[1] & AF1[0] & TIME);
+    assign FF1[1] = (~AF1[2] & ~AF1[1] & AF1[0] & M1) | (~AF1[2] & AF1[1] & ~AF1[0] & ~M1) | (~AF1[2] & AF1[1] & AF1[0] & ~TIME) | (~AF1[2] & ~AF1[1] & ~AF1[0] & SB1[1] & ~SB1[0]);
+    assign FF1[0] = (~AF1[2] & AF1[0]) | (~AF1[2] & ~AF1[1] & ~SB1[1] & SB1[0]) | (AF1[2] & ~AF1[1] & ~AF1[0] & TIME2);
+    assign DONE   = (~AF1[2] & AF1[1] & AF1[0] & TIME) | (AF1[2] & ~AF1[1] & ~AF1[0] & TIME2);
+    assign ENABLE1 = (~AF1[2] & AF1[1] & AF1[0]) | (AF1[2] & ~AF1[1] & ~AF1[0]);
 
+endmodule 
--- Progra_DIGI/FSM_tb.v
+++ Progra_DIGI/FSM_tb.v
@@ -8,17 +8,30 @@
 
 module testbench();
 
-reg clk, reset, set,  ON, CONT; 
-reg [1:0]SELE;
-wire TIME;
-wire [2:0]A;
-wire [2:0]F;
-wire [2:0]DISP;
-wire [2:0]MENU;
-wire ENABLE;
+reg clk, reset, set,  ON, CONT, CONTm1, CONTm2, CONTm3;
 
-FSM_BIENVENIDA FSM(clk, reset, F, A, ON, CONT, TIME, SELE, DISP, MENU, ENABLE);
+wire TIME;       wire TIME2; 
+reg [1:0]SELE;   reg [2:0]SELEm1;   reg [2:0]SELEm2;    reg [2:0]SELEm3;        reg [1:0]SELEB1;        reg [1:0]SELEB2;        reg [1:0]SELEB3;        reg [1:0]SELEB4;        reg M1;
+wire [2:0]A;     wire [2:0]Am1;     wire [2:0]Am2;      wire [2:0]Am3;          wire [2:0]AB1;          wire [2:0]AB2;          wire [2:0]AB3;          wire [2:0]AB4;          wire [2:0]AF1;
+wire [2:0]F;     wire [2:0]Fm1;     wire [2:0]Fm2;      wire [2:0]Fm3;          wire [2:0]FB1;          wire [2:0]FB2;          wire [2:0]FB3;          wire [2:0]FB4;          wire [2:0]FF1;
+wire [2:0]MENU;  wire [3:0]OP;      wire [3:0]G;        wire [3:0]B;            wire [1:0]SB1;          wire [1:0]SB2;          wire [1:0]SB3;          wire [1:0]SB4;          wire DONE;
+wire ENABLE;     wire ENABLE1;
+
+
+FSM_BIENVENIDA FSM0 (clk, reset, F, A, ON, CONT, TIME, SELE, MENU, ENABLE);
+FSM_MENU1 FSM1 (clk, reset, Fm1, Am1, MENU, CONTm1, TIME, SELEm1, OP, ENABLE);
+FSM_MENU2 FSM2 (clk, reset, Fm2, Am2, OP, CONTm2, TIME, SELEm2, G, ENABLE);
+FSM_MENU3 FMS3 (clk, reset, Fm3, Am3, G, CONTm3, TIME, SELEm3, B, ENABLE);
+FSM_B1 B1 (clk, reset, FB1, AB1, B, SELEB1, SB1);
+FSM_B2 B2 (clk, reset, FB2, AB2, B, SELEB2, SB2);
+FSM_B3 B3 (clk, reset, FB3, AB3, B, SELEB3, SB3);
+FSM_B4 B4 (clk, reset, FB4, AB4, B, SELEB4, SB4);
+FSM_LLENADOB1 B_1 (clk, reset, FF1, AF1, TIME, TIME2, SB1, M1, DONE, ENABLE1);
+
 Timer TIM(ENABLE, TIME);
+Timer2 TIM2(ENABLE1, TIME2);
+
+
 initial begin
     clk=1;
     forever #5 clk = ~clk;
@@ -28,145 +41,80 @@ end
     
         #1 
         $display (" ");
-        $display(" Proyecto 1 - FSM");
-        $display("    A     ON    CONT   TIME    SELE | F      MENU     DISP   ENABLE ");
-        $display(" --------------------------------| ---------------------------- ");
-        $monitor(" %b    %b      %b      %b       %b | %b     %b     %b      %b  ", A, ON, CONT, TIME, SELE, F, MENU, DISP, ENABLE);
-
-            reset = 0 ; ON = 0 ; CONT = 0; SELE = 2'b00; 
+        $display("Proyecto 1 - FSM");
+        $display("");
+        $display(" ON  CONT  CONTm1  CONTm2  CONTm3  SELE  SELEm1  SELEm2  SELEm3  SELEB1  SELEB2  SELEB3  SELEB4  MENU   OP     G      B    SB1  SB2  SB3  SB4  M1 | DONE  ");
+        $display(" -------------------------------------------------------------------------------------------------------------------------------------------------|-------");
+        $monitor(" %b    %b      %b       %b       %b      %b     %b     %b    %b      %b      %b      %b      %b    %b   %b   %b   %b   %b   %b   %b   %b   %b |   %b     ", ON, CONT, CONTm1, CONTm2, CONTm3, SELE, SELEm1, SELEm2, SELEm3, SELEB1, SELEB2, SELEB3, SELEB4, MENU, OP, G, B, SB1, SB2, SB3, SB4, M1, DONE);
+       
+            reset = 0 ; ON = 0 ; CONT = 0; SELE = 2'b00; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
         #1  reset = 1 ;
         #1  reset = 0 ; 
 
+       
         // Prueba de que la Interfaz de Bienvenida (Seleccionador de menús) solo se activa con "ON = 1"
-        #7   ON = 0 ; CONT = 0 ; SELE = 2'b00 ; 
-        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ; 
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ; 
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b10 ;  
-        #10  ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b11 ;    
 
+        #7   ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
+        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b10 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
+        #10  ON = 1 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;  // Máquina encendida
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b11 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
+
+        $display("");
         #5  reset = 1 ;
         #5  reset = 0 ;
-
+        
         // Prueba de que el timer funciona correctamente.
 
-        #0   ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b11 ;
+        #0   ON = 1 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;  // Máquina encendida
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b11 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;
 
+        $display("");
         #5  reset = 1 ;
         #5  reset = 0 ; 
-        
-        // Probando elección de Menú 1 - Platos Fuertes
-        #0   ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; 
-    
+
+        // Probando funcionamiento completo de la máquina:
+
+        #7   ON = 1 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Encendido de máquina
+        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Continuar a selección de menús
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Elección de Menú 1 - Platos Fuertes
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Transición
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 1 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Continuar a selección de Platos Fuertes 
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b011 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ;// Elección de Plato Fuerte #3 - Lasagna 
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Transición
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 1 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Continuar a selección de Guarniciones
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b100 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Elección de Guarnición #4 - Ensalada y pure de papa
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Transición
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 1 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Continuar a selección de Bebidas
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b001 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Elección de Bebida #1
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Transición 
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b10 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Seleccionando tamaño pequeño
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Transición 
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 1 ; // Encendido de motor para llenado de bebida
+
+        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; CONTm1 = 0 ; SELEm1 = 3'b000 ; CONTm2 = 0 ; SELEm2 = 3'b000 ; CONTm3 = 0 ; SELEm3 = 3'b000 ; SELEB1 = 2'b00 ; SELEB2 = 2'b00 ; SELEB3 = 2'b00 ; SELEB4 = 2'b00 ; M1 = 0 ; // Transición  
+
+
+        $display("");
         #5  reset = 1 ;
         #5  reset = 0 ;
 
-        // Probando elección de Menú 2 - Guarniciones 
-        #0   ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b10 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; 
-
-        #5  reset = 1 ;
-        #5  reset = 0 ;
-
-        // Probando elección de Menú 3 - Bebidas
-        #0   ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b11 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; 
-
-        #5  reset = 1 ;
-        #5  reset = 0 ;
-
-
-
-
-
-        // Prueba de transción a Menú 1 - Platos Fuertes
-
-       /* #10  ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 1 ; SELE = 2'b00 ;
-        #10  ON = 0 ; CONT = 0 ; SELE = 2'b01 ;
-/*
-        // Prueba de transición a Menú 2 - Guarniciones
-        #10 ON = 1 ; CONT = 0 ; SELE = 2'b00 ; 
-        #10 ON = 0 ; CONT = 1 ; SELE = 2'b00 ;
-        #10 ON = 0 ; CONT = 0 ; SELE = 2'b10 ;
-
-        // Prueba de transición a Menú 3 - Bebidas
-        #10 ON = 1 ; CONT = 0 ; SELE = 2'b00 ;
-        #10 ON = 0 ; CONT = 1 ; SELE = 2'b00 ;
-        #10 ON = 0 ; CONT = 1 ; SELE = 2'b11 ; 
-
-        
-        #16  ON = 0 ; CONT = 1 ; TIME = 0 ; SELE = 2'b00 ;
-        #26  ON = 0 ; CONT = 0 ; TIME = 1 ; SELE = 2'b00 ;
-        #36  ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b00 ;
-        #46  ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b01 ;
-        #56  ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b10 ;
-        #66  ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b11 ;
-
-        // Prueba de que el timer funciona, regresa de S0 a SR si "CONT = 0" y "TIME = 1" 
-        #76  ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b00 ;  // S0
-        #86  ON = 0 ; CONT = 0 ; TIME = 1 ; SELE = 2'b00 ;  // SR
-
-        // Probando selección de menú 1 - Platos fuertes
-        #96  ON = 1 ; CONT = 0 ; TIME = 0 ; SELE = 2'b00 ; // S0 
-        #106 ON = 0 ; CONT = 1 ; TIME = 0 ; SELE = 2'b00 ; // S1 
-        #116 ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b01 ; // S2 - Menú 1 y regresa a SR 
-
-        // Probando selección de menú 2 - Guarniciones
-        #126 ON = 1 ; CONT = 0 ; TIME = 0 ; SELE = 2'b00; // S0
-        #136 ON = 0 ; CONT = 1 ; TIME = 0 ; SELE = 2'b00; // S1
-        #146 ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b10; // S2 - Menú 2
-        
-        // Probando selección de menú 3 - Bebidas 
-        #156 ON = 1 ; CONT = 0 ; TIME = 0 ; SELE = 2'b00; // S0
-        #166 ON = 0 ; CONT = 1 ; TIME = 0 ; SELE = 2'b00; // S1
-        #176 ON = 0 ; CONT = 0 ; TIME = 0 ; SELE = 2'b11; // S3 - Menú 3
-*/
-
-
-
-
-
-        /*
-        #6  ON = 0 ; CONT = 0 ; SELE = 2'b01 ; // SR
-        #6  ON = 0 ; CONT = 0 ; SELE = 2'b10 ; // SR
-        #6  ON = 0 ; CONT = 0 ; SELE = 2'b11 ; // SR*/
-
-/*
-        // Prueba de que el timer funciona, regresa de S0 a SR si "CONT = 0" y "TIME = 1" 
-        #6  ON = 1 ; CONT = 0 ; SELE = 2'b00 ; // S0
-        #6  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; // S0
-        #6  ON = 0 ; CONT = 0 ; SELE = 2'b00 ; // SR
-
-        // Probando selección de menú 1 - Platos fuertes
-        #6 ON = 1 ; CONT = 0 ; SELE = 2'b00 ; // S0 
-        #6 ON = 0 ; CONT = 1 ; SELE = 2'b00 ; // S1 
-        #6 ON = 0 ; CONT = 0 ; SELE = 2'b01 ; // S2 - Menú 1 y regresa a SR 
-
-        // Probando selección de menú 2 - Guarniciones
-        #6 ON = 1 ; CONT = 0 ; SELE = 2'b00; // S0
-        #6 ON = 0 ; CONT = 1 ; SELE = 2'b00; // S1
-        #6 ON = 0 ; CONT = 0 ; SELE = 2'b10; // S2 - Menú 2
-        
-        // Probando selección de menú 3 - Bebidas 
-        #6 ON = 1 ; CONT = 0 ; SELE = 2'b00; // S0
-        #6 ON = 0 ; CONT = 1 ; SELE = 2'b00; // S1
-        #6 ON = 0 ; CONT = 0 ; SELE = 2'b11; // S3 - Menú 3
-
-*/
     end
 
     initial
-    #250  $finish;
+    #200  $finish;
 
 initial
     begin
@@ -174,8 +122,3 @@ initial
         $dumpvars(0, testbench);
     end
 endmodule
-
-
-
-
-
